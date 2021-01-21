05b2b962d1c81d2a94814f0532790bc7265b5ecc
wangjiajia <wangjiajia_cumt@163.com>
2021-01-21 16:09:35 +0800

    平面检测注释

--- apps/floor_detection_nodelet.cpp
+++ apps/floor_detection_nodelet.cpp
@@ -38,7 +38,7 @@ public:
     private_nh = getPrivateNodeHandle();
 
     initialize_params();
-
+    // 接收滤波后的点云数据
     points_sub = nh.subscribe("/filtered_points", 256, &FloorDetectionNodelet::cloud_callback, this);
     floor_pub = nh.advertise<hdl_graph_slam::FloorCoeffs>("/floor_detection/floor_coeffs", 32);
 
@@ -79,6 +79,8 @@ private:
     boost::optional<Eigen::Vector4f> floor = detect(cloud);
 
     // publish the detected floor coefficients
+    // 平面 方程 ax+by+cz+d = 0;
+    // 系数包含4个
     hdl_graph_slam::FloorCoeffs coeffs;
     coeffs.header = cloud_msg->header;
     if(floor) {
@@ -107,37 +109,47 @@ private:
    */
   boost::optional<Eigen::Vector4f> detect(const pcl::PointCloud<PointT>::Ptr& cloud) const {
     // compensate the tilt rotation
+    // 根据激光雷达的实际安装倾斜角度，将坐标转换为汽车坐标系
     Eigen::Matrix4f tilt_matrix = Eigen::Matrix4f::Identity();
     tilt_matrix.topLeftCorner(3, 3) = Eigen::AngleAxisf(tilt_deg * M_PI / 180.0f, Eigen::Vector3f::UnitY()).toRotationMatrix();
 
     // filtering before RANSAC (height and normal filtering)
+    // 默认认为地面理论上应该在与传感器高度一致附近，则将其他非地面附近的点云滤除
     pcl::PointCloud<PointT>::Ptr filtered(new pcl::PointCloud<PointT>);
     pcl::transformPointCloud(*cloud, *filtered, tilt_matrix);
     filtered = plane_clip(filtered, Eigen::Vector4f(0.0f, 0.0f, 1.0f, sensor_height + height_clip_range), false);
     filtered = plane_clip(filtered, Eigen::Vector4f(0.0f, 0.0f, 1.0f, sensor_height - height_clip_range), true);
 
+    // 采用法向量判断是否与z轴夹角较小，否则为异常点剔除
     if(use_normal_filtering) {
       filtered = normal_filtering(filtered);
     }
 
+    // 还原坐标系
     pcl::transformPointCloud(*filtered, *filtered, static_cast<Eigen::Matrix4f>(tilt_matrix.inverse()));
 
+    // 发布滤波后的点云
     if(floor_filtered_pub.getNumSubscribers()) {
       filtered->header = cloud->header;
       floor_filtered_pub.publish(filtered);
     }
 
     // too few points for RANSAC
+    // 有效点云个数不足以形成平面
     if(filtered->size() < floor_pts_thresh) {
       return boost::none;
     }
 
-    // RANSAC
+    // RANSAC 算法拟合平面
+    // 平面模型
     pcl::SampleConsensusModelPlane<PointT>::Ptr model_p(new pcl::SampleConsensusModelPlane<PointT>(filtered));
+    // RANSAC 算法拟合
     pcl::RandomSampleConsensus<PointT> ransac(model_p);
+    // 误差范围0.1m
     ransac.setDistanceThreshold(0.1);
     ransac.computeModel();
 
+    // 获取满足条件的特征点个数，即所谓内点个数
     pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
     ransac.getInliers(inliers->indices);
 
@@ -149,9 +161,11 @@ private:
     // verticality check of the detected floor's normal
     Eigen::Vector4f reference = tilt_matrix.inverse() * Eigen::Vector4f::UnitZ();
 
+    // 求出平面方程
     Eigen::VectorXf coeffs;
     ransac.getModelCoefficients(coeffs);
 
+    // 计算出平面应与实际地面夹角小于一定范围，否则地面计算错误
     double dot = coeffs.head<3>().dot(reference.head<3>());
     if(std::abs(dot) < std::cos(floor_normal_thresh * M_PI / 180.0)) {
       // the normal is not vertical
@@ -159,10 +173,12 @@ private:
     }
 
     // make the normal upward
+    // 确保地面法线朝上
     if(coeffs.head<3>().dot(Eigen::Vector3f::UnitZ()) < 0.0f) {
       coeffs *= -1.0f;
     }
 
+    // 发布平面点云
     if(floor_points_pub.getNumSubscribers()) {
       pcl::PointCloud<PointT>::Ptr inlier_cloud(new pcl::PointCloud<PointT>);
       pcl::ExtractIndices<PointT> extract;
@@ -174,6 +190,7 @@ private:
       floor_points_pub.publish(inlier_cloud);
     }
 
+    // 获取平面方程
     return Eigen::Vector4f(coeffs);
   }
 
@@ -183,15 +200,19 @@ private:
    * @param plane
    * @param negative
    * @return
+   * 简单的点云剔除功能，即满足条件的保留原点云
+   * 采用一个平面进行2分割
    */
   pcl::PointCloud<PointT>::Ptr plane_clip(const pcl::PointCloud<PointT>::Ptr& src_cloud, const Eigen::Vector4f& plane, bool negative) const {
+    // 点云裁剪类对象 平面模型
     pcl::PlaneClipper3D<PointT> clipper(plane);
     pcl::PointIndices::Ptr indices(new pcl::PointIndices);
 
+    // 满足点云索引
     clipper.clipPointCloud3D(*src_cloud, indices->indices);
 
     pcl::PointCloud<PointT>::Ptr dst_cloud(new pcl::PointCloud<PointT>);
-
+    // 剔除
     pcl::ExtractIndices<PointT> extract;
     extract.setInputCloud(src_cloud);
     extract.setIndices(indices);
@@ -205,14 +226,18 @@ private:
    * @brief filter points with non-vertical normals
    * @param cloud  input cloud
    * @return filtered cloud
+   * 根据每个点法线进行滤波，即法线与z轴夹角过大则滤除
    */
   pcl::PointCloud<PointT>::Ptr normal_filtering(const pcl::PointCloud<PointT>::Ptr& cloud) const {
+    // pcl 求法线
     pcl::NormalEstimation<PointT, pcl::Normal> ne;
     ne.setInputCloud(cloud);
 
+    // 搜索方法采用kdtree
     pcl::search::KdTree<PointT>::Ptr tree(new pcl::search::KdTree<PointT>);
     ne.setSearchMethod(tree);
 
+    // 计算法线，搜索范围内的10个点
     pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
     ne.setKSearch(10);
     ne.setViewPoint(0.0f, 0.0f, sensor_height);
@@ -221,6 +246,8 @@ private:
     pcl::PointCloud<PointT>::Ptr filtered(new pcl::PointCloud<PointT>);
     filtered->reserve(cloud->size());
 
+    // 遍历所有点的法线，法线与z轴夹角大于一定值，则剔除
+    // 即cos大于阈值角度cos则保留
     for(int i = 0; i < cloud->size(); i++) {
       float dot = normals->at(i).getNormalVector3fMap().normalized().dot(Eigen::Vector3f::UnitZ());
       if(std::abs(dot) > std::cos(normal_filter_thresh * M_PI / 180.0)) {
