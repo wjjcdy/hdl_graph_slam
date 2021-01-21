3113ca27a3ddbae2eb75d89ba473b878291ca250
wangjiajia <wangjiajia_cumt@163.com>
2021-01-21 11:28:34 +0800

    增加滤波注释

--- apps/prefiltering_nodelet.cpp
+++ apps/prefiltering_nodelet.cpp
@@ -51,6 +51,9 @@ private:
     std::string downsample_method = private_nh.param<std::string>("downsample_method", "VOXELGRID");
     double downsample_resolution = private_nh.param<double>("downsample_resolution", 0.1);
 
+    // 两种滤波方式，体素滤波和近似体素滤波，均采用pcl库
+    // 体素滤波，即采用一个体素中所有点的均值替代
+    // 近似体素滤波，即采用一个体素的中心点替代
     if(downsample_method == "VOXELGRID") {
       std::cout << "downsample: VOXELGRID " << downsample_resolution << std::endl;
       boost::shared_ptr<pcl::VoxelGrid<PointT>> voxelgrid(new pcl::VoxelGrid<PointT>());
@@ -69,6 +72,9 @@ private:
       std::cout << "downsample: NONE" << std::endl;
     }
 
+    // 两种孤立噪点滤波功能，pcl库
+    // StatisticalOutlierRemoval: 临近点采用高斯分布，距离超出标准差的剔除
+    // RadiusOutlierRemoval: 当前点阈值距离范围内其他点个数较少，则剔除
     std::string outlier_removal_method = private_nh.param<std::string>("outlier_removal_method", "STATISTICAL");
     if(outlier_removal_method == "STATISTICAL") {
       int mean_k = private_nh.param<int>("statistical_mean_k", 20);
@@ -99,6 +105,7 @@ private:
     base_link_frame = private_nh.param<std::string>("base_link_frame", "");
   }
 
+  // 高速接收IMU数据，并放入队列中
   void imu_callback(const sensor_msgs::ImuConstPtr& imu_msg) {
     imu_queue.push_back(imu_msg);
   }
@@ -108,6 +115,7 @@ private:
       return;
     }
 
+    // 运动畸变矫正
     src_cloud = deskewing(src_cloud);
 
     // if base_link_frame is defined, transform the input cloud to the frame
@@ -126,14 +134,17 @@ private:
       transformed->header.stamp = src_cloud->header.stamp;
       src_cloud = transformed;
     }
-
+    // 有效距离滤波
     pcl::PointCloud<PointT>::ConstPtr filtered = distance_filter(src_cloud);
+    // 降采样
     filtered = downsample(filtered);
+    // 噪点滤波
     filtered = outlier_removal(filtered);
 
     points_pub.publish(filtered);
   }
 
+  // 原始点云降采样
   pcl::PointCloud<PointT>::ConstPtr downsample(const pcl::PointCloud<PointT>::ConstPtr& cloud) const {
     if(!downsample_filter) {
       return cloud;
@@ -147,6 +158,7 @@ private:
     return filtered;
   }
 
+  // 孤立等噪点滤波
   pcl::PointCloud<PointT>::ConstPtr outlier_removal(const pcl::PointCloud<PointT>::ConstPtr& cloud) const {
     if(!outlier_removal_filter) {
       return cloud;
@@ -160,6 +172,7 @@ private:
     return filtered;
   }
 
+  // 滤除较远和较近的点云
   pcl::PointCloud<PointT>::ConstPtr distance_filter(const pcl::PointCloud<PointT>::ConstPtr& cloud) const {
     pcl::PointCloud<PointT>::Ptr filtered(new pcl::PointCloud<PointT>());
     filtered->reserve(cloud->size());
@@ -178,8 +191,13 @@ private:
     return filtered;
   }
 
+  // 点云去运动畸变，？？？？？？？
+  // 多线激光雷达，理论上应该是垂直同时，旋转扫描
+  // 矫正的时间间隔，不知为何，却是以点云的顺序进行全部计算时刻，进行校准
   pcl::PointCloud<PointT>::ConstPtr deskewing(const pcl::PointCloud<PointT>::ConstPtr& cloud) {
+    // 获取点云接收的时间戳
     ros::Time stamp = pcl_conversions::fromPCL(cloud->header.stamp);
+    // 无imu新数据，不畸变矫正
     if(imu_queue.empty()) {
       return cloud;
     }
@@ -193,6 +211,7 @@ private:
       colored->height = cloud->height;
       colored->resize(cloud->size());
 
+      // 每个点云根据扫描顺序显示颜色，方便观察
       for(int i = 0; i < cloud->size(); i++) {
         double t = static_cast<double>(i) / cloud->size();
         colored->at(i).getVector4fMap() = cloud->at(i).getVector4fMap();
@@ -203,8 +222,10 @@ private:
       colored_pub.publish(colored);
     }
 
+    // 获取imu数据最老数据
     sensor_msgs::ImuConstPtr imu_msg = imu_queue.front();
 
+    // 遍历imu队列中的，根据时间戳，找到时间戳一致的imu数据
     auto loc = imu_queue.begin();
     for(; loc != imu_queue.end(); loc++) {
       imu_msg = (*loc);
@@ -213,11 +234,15 @@ private:
       }
     }
 
+    // 剔除已遍历后的imu数据
     imu_queue.erase(imu_queue.begin(), loc);
 
+    // 获取当前角速度
     Eigen::Vector3f ang_v(imu_msg->angular_velocity.x, imu_msg->angular_velocity.y, imu_msg->angular_velocity.z);
+    // 反向
     ang_v *= -1;
 
+    // 初始化畸变矫正后的点云数据
     pcl::PointCloud<PointT>::Ptr deskewed(new pcl::PointCloud<PointT>());
     deskewed->header = cloud->header;
     deskewed->is_dense = cloud->is_dense;
@@ -230,8 +255,11 @@ private:
       const auto& pt = cloud->at(i);
 
       // TODO: transform IMU data into the LIDAR frame
+      // 计算每个点云时刻相对于起始点的时刻
       double delta_t = scan_period * static_cast<double>(i) / cloud->size();
+      // 根据匀角速度变换，计算每个点的角度姿态角
       Eigen::Quaternionf delta_q(1, delta_t / 2.0 * ang_v[0], delta_t / 2.0 * ang_v[1], delta_t / 2.0 * ang_v[2]);
+      // 矫正畸变
       Eigen::Vector3f pt_ = delta_q.inverse() * pt.getVector3fMap();
 
       deskewed->at(i) = cloud->at(i);
